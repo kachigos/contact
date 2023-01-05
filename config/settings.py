855e89e6d57c29a7b164288cf69d2eb7d9047251
kachigos <kachigos@C02WF085HV2T.grubhub.local>
2023-01-05 19:34:27 +0600

    n

--- config/settings.py
+++ config/settings.py
@@ -137,8 +137,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SITE_ID = 1
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'chyngyzsubhanov@gmail.com'
+EMAIL_HOST_PASSWORD = 'hmbfrviopzdfunsp'
 EMAIL_PORT = 587
 EMAIL_USE_TLS   = True
 ACTIVATE_USERS_EMAIL = True
